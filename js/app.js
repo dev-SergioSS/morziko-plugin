2da268af661a497e5a4da208ee86ba4b39b1443a
dev-SergioSS <sannikov.ua@gmail.com>
2022-08-09 17:38:03 +0300

    add test PWA

--- js/app.js
+++ js/app.js
@@ -109,3 +109,14 @@ function stopTimer() {
 }
 
 setStartTimerValue();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/js/sw.js')
+    .then(() =>
+      navigator.serviceWorker.ready.then((worker) => {
+        worker.sync.register('syncdata');
+      }),
+    )
+    .catch((err) => console.log(err));
+}
